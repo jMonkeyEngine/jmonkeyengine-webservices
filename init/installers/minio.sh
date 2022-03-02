1b2023068a39f2eb66f0a69c688c73e86d49bafc
riccardobl <riccardo0blb@gmail.com>
2022-03-02 09:40:00 +0100

    minio: fix upgrade issue, better health checks

--- init/installers/minio.sh
+++ init/installers/minio.sh
@@ -19,8 +19,15 @@ docker run  --restart=always   -d  --name minio \
   -v /srv/minio/config:/root/.minio \
   --read-only \
   --tmpfs  /tmp:uid=1000,gid=1000 \
-  --health-cmd="curl -f http://localhost:9000/minio/health/live || exit 1" \
-  minio/minio server  /data --console-address "9001"
+  --health-cmd="(\
+(curl -f http://localhost:9000/minio/health/live)\
+&&(curl -f https://${OBJECT_STORAGE_HOSTNAME}/minio/health/live)\
+&&(curl -f https://${ARTIFACTS_HOSTNAME}/|grep jmonkeyengine)\
+&&(curl -f https://${OBJECT_STORAGE_HOSTNAME}/native-snapshots|grep natives)\
+&&(curl https://${OBJECT_STORAGE_CONSOLE_HOSTNAME} | grep doctype)\
+)|| exit 1" \
+minio/minio server  /data --console-address "0.0.0.0:9001"
 
 docker network connect --alias minio.docker nginx_gateway_net minio  
 docker network disconnect bridge minio
+docker restart nginx_gateway||true
